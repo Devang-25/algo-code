450e6e768496aab20a8db8f1cb649110c1e05713
octo-octavius <sekharan.natarajan@gmail.com>
2018-07-09 15:36:16 -0700

    auto commit

---
+++ src/leetcode/medium/LexicographicalNumbers.java
@@ -0,0 +1,63 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mns on 7/5/18.
+ */
+public class LexicographicalNumbers {
+//    public List<Integer> lexicalOrder(int n) {
+//        List<Integer> result = new ArrayList<>();
+//        StringBuilder sb = new StringBuilder(n);
+//        for (int i = 1; i <= 9; i++) {
+//            sb.append(i);
+//            lexUtil(sb, n, result);
+//            sb.setLength(sb.length() - 1);
+//
+//        }
+//        return result;
+//    }
+//
+//    private void lexUtil(StringBuilder sb, int n, List<Integer> result) {
+//        Integer val = Integer.parseInt(sb.toString());
+//        if (val > n) {
+//            return;
+//        }
+//
+//        result.add(val);
+//        for (int i = 0; i <= 9; i++) {
+//            if (sb.length() < String.valueOf(n).length()) {
+//                sb.append(i);
+//                lexUtil(sb, n, result);
+//                sb.setLength(sb.length() - 1);
+//            }
+//        }
+//    }
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            if(i <= n) {
+                lexUtil(i, n, result);
+            }
+        }
+        return result;
+    }
+
+    private void lexUtil(int curr, int n, List<Integer> result) {
+        result.add(curr);
+        for (int i = 0; i <= 9; i++) {
+            if(curr*10+i <= n){
+                lexUtil(curr*10+i,n,result);
+            }else{
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LexicographicalNumbers l = new LexicographicalNumbers();
+        l.lexicalOrder(123).stream().forEach(System.out::println);
+    }
+}
---
+++ src/leetcode/medium/MinimumTimeDiff.java
@@ -0,0 +1,44 @@
+package medium;
+
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.List;
+
+/**
+ * Created by mns on 7/2/18.
+ */
+public class MinimumTimeDiff {
+    public int findMinDifference(List<String> timePoints) {
+        BitSet bt = new BitSet(24*60);
+        for(String st  : timePoints){
+            int hours = Integer.parseInt(st.substring(0,2));
+            int minutes = hours*60+Integer.parseInt(st.substring(3));
+            if(bt.get(minutes)){
+                return 0;
+            }
+            bt.set(minutes);
+        }
+
+        int index = bt.nextSetBit(0);
+        int first = bt.nextSetBit(0);
+        int minDiff = Integer.MAX_VALUE;
+        while(index < bt.size()){
+            int next = bt.nextSetBit(index+1);
+            if(next!=-1){
+                minDiff = Math.min(next - index, minDiff);
+                index = next;
+            }else{
+                minDiff = Math.min(24*60+first - index,minDiff);
+                break;
+            }
+        }
+
+        return minDiff;
+    }
+
+    public static void main(String[] args) {
+        MinimumTimeDiff mtd = new MinimumTimeDiff();
+        System.out.println(mtd.findMinDifference(Arrays.asList("23:59","00:00")));
+        System.out.println(mtd.findMinDifference(Arrays.asList("05:31","22:08","00:35")));
+    }
+}
