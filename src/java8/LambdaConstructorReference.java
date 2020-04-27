86536ba0ce2ce82a9c4cec85285c8ba697d7e3a6
octo-octavius <39812034+octo-octavius@users.noreply.github.com>
2020-04-26 23:42:31 -0700

    auto commit

--- src/java8/LambdaConstructorReference.java
+++ src/java8/LambdaConstructorReference.java
@@ -20,20 +20,20 @@ public class LambdaConstructorReference {
         }
     }
 
-    public static ArrayList<Person> extractNamesIntoAList(ArrayList<Person> l, Function<String, Person> ctor) {
-        ArrayList<Person> list = new ArrayList<>();
-        l.forEach(p -> list.add(ctor.apply(p.name + ":(")));
+    public static ArrayList < Person > extractNamesIntoAList(ArrayList < Person > l, Function < String, Person > ctor) {
+        ArrayList < Person > list = new ArrayList < > ();
+        l.forEach(p-> list.add(ctor.apply(p.name + ":(")));
         return list;
     }
 
     public static void main(String[] args) {
-        ImmutableList<Person> list = ImmutableList.of(new Person("A"), new Person("B"), new Person("C"));
+        ImmutableList < Person > list = ImmutableList.of(new Person("A"), new Person("B"), new Person("C"));
 
-        ArrayList<Person> list2 = new ArrayList<>(
-                list.stream().map((person) -> person.name + ":)").map(Person::new).collect(Collectors.toList()));
+        ArrayList < Person > list2 = new ArrayList < > (
+            list.stream().map((person) -> person.name + ":)").map(Person::new).collect(Collectors.toList()));
         list2.forEach(System.out::println);
 
-        ArrayList<Person> list3 = extractNamesIntoAList(list2, Person::new);
+        ArrayList < Person > list3 = extractNamesIntoAList(list2, Person::new);
         list3.forEach(System.out::println);
 
     }
