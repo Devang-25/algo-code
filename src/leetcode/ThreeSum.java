24dedbeca0d946fc690482ca2683c5c60b44eb35
Sekharan Natarajan <mnsekh@mnsekh-macbookpro.roam.corp.google.com>
2020-05-16 00:28:42 -0700

    auto commit

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -17,48 +17,48 @@ import java.util.TreeSet;
  */
 public class ThreeSum {
 
-    public static class Solution {
+  public static class Solution {
 
-        public class CustomComparator implements Comparator<List<Integer>> {
+    public class CustomComparator implements Comparator<List<Integer>> {
 
-            @Override
-            public int compare(List<Integer> o1, List<Integer> o2) {
-                if (o1.containsAll(o2) && o2.containsAll(o1)) {
-                    return 0;
-                } else if (o1.containsAll(o2) && !o2.containsAll(o1)) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            }
-
+      @Override
+      public int compare(List<Integer> o1, List<Integer> o2) {
+        if (o1.containsAll(o2) && o2.containsAll(o1)) {
+          return 0;
+        } else if (o1.containsAll(o2) && !o2.containsAll(o1)) {
+          return 1;
+        } else {
+          return -1;
         }
+      }
 
-        public List<List<Integer>> threeSum(int[] nums) {
-            TreeSet<List<Integer>> output = new TreeSet<>(new CustomComparator());
-            Arrays.sort(nums);
-
-            for (int i = 0; i < nums.length - 1; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    int sum = nums[i] + nums[j];
-                    int complement = Arrays.binarySearch(nums, -sum);
-                    if (!(complement <0) && complement != i && complement != j) {
-
-                        int[] list = {nums[i], nums[j], nums[complement]};
-                        Arrays.sort(list);
-                        output.add(new ArrayList(Arrays.asList(list)));
-                    }
-                }
-            }
-
-            ArrayList<List<Integer>> finalList = new ArrayList<>(output);
-            return finalList;
-        }
     }
 
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int [] inp = {-1, 0, 1, 2, -1, -4};
-        s.threeSum(inp);
+    public List<List<Integer>> threeSum(int[] nums) {
+      TreeSet<List<Integer>> output = new TreeSet<>(new CustomComparator());
+      Arrays.sort(nums);
+
+      for (int i = 0; i < nums.length - 1; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+          int sum = nums[i] + nums[j];
+          int complement = Arrays.binarySearch(nums, -sum);
+          if (!(complement < 0) && complement != i && complement != j) {
+
+            int[] list = { nums[i], nums[j], nums[complement] };
+            Arrays.sort(list);
+            output.add(new ArrayList(Arrays.asList(list)));
+          }
+        }
+      }
+
+      ArrayList<List<Integer>> finalList = new ArrayList<>(output);
+      return finalList;
     }
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    int[] inp = { -1, 0, 1, 2, -1, -4 };
+    s.threeSum(inp);
+  }
 }
